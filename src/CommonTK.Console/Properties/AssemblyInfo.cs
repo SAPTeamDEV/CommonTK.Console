c40c6997a947e70fdd54d33b05c38b84c5c04793
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-03-30 10:40:52 +0330

    Add strong naming key

--- src/CommonTK.Console/Properties/AssemblyInfo.cs
+++ src/CommonTK.Console/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SAP.CommonTK.Console")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("Common Toolkit")]
-[assembly: AssemblyCompany("SAP Team")]
-[assembly: AssemblyCopyright("Copyright © SAP Team 2022")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
