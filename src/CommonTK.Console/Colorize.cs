d3fc3618d1eae0b1f64ebbb34405a4c10b3834ac
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-03-31 22:44:26 +0330

    Drop support of net40 due to incompatibilities

--- src/CommonTK.Console/Colorize.cs
+++ src/CommonTK.Console/Colorize.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-#if NET461
+#if NET452
 using IEnumerable.Append;
 #endif
 
--- src/CommonTK.Console/ConsoleLaunchMode.cs
+++ src/CommonTK.Console/ConsoleLaunchMode.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SAPTeam.CommonTK.Console
+﻿namespace SAPTeam.CommonTK.Console
 {
     /// <summary>
     /// Represents the Console launching mechanisms.
